f2e203ea4ef9ee79fdae01ed5b3c0a14104e2497
Sreelekshmiust <127503506+Sreelekshmiust@users.noreply.github.com>
2024-03-22 13:07:37 +0530

    Add files via upload

--- JavaAssessment/src/question1/EmployeeAddress.java
+++ JavaAssessment/src/question1/EmployeeAddress.java
@@ -2,6 +2,8 @@ package question1;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeAddress {
 
@@ -9,7 +11,7 @@ public class EmployeeAddress {
         Map<Employee, Address> employeeAddressMap = new HashMap<>();
         employeeAddressMap.put(new Employee(1, "Sree"),new Address(101, "kulathoor", "trivandrum", 12345));
         employeeAddressMap.put(new Employee(2, "Akshaya"),new Address(102, "kazhakootam", "trivandrum", 12343));
-        employeeAddressMap.put(new Employee(3, "Abhi"),new Address(103, "kunnil", "trivandrum", 12355));
+        employeeAddressMap.put(new Employee(3, "Abhi"),new Address(103, "kunnil", "ernakulam", 12355));
         employeeAddressMap.put(new Employee(4, "Christo"),new Address(104, "kunnil", "trivandrum", 12355));
 
         Employee employee = new Employee(4, "Christo");
@@ -20,8 +22,13 @@ public class EmployeeAddress {
         } else {
             System.out.println("Address not found for Employee " + employee.getEmployeeId());
         }
+    Set<String> cities = new TreeSet<>();
+        for (Address a : employeeAddressMap.values()) {
+        cities.add(a.getCity());
     }
 
+        System.out.println("Distinct cities in ascending order: " + cities);
+}
 
     public Address getAddress(Map<Employee, Address> employeeAddressMap, Employee employee) {
         return employeeAddressMap.get(employee);
